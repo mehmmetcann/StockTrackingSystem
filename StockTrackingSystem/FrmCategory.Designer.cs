b313742c1d46089fd7c1cb4e7dd8b081a5686fcf
mehmetcan yıldırım <mehmetcan_3566@hotmail.com>
2026-02-11 20:11:39 +0300

    add new folders to project

---
+++ StockTrackingSystem/FrmCategory.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace StockTrackingSystem
+{
+    partial class FrmCategory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridViewCategories = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCategoryName = new System.Windows.Forms.TextBox();
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.txtCategoryId = new System.Windows.Forms.TextBox();
+            this.panelLeft = new System.Windows.Forms.Panel();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.lblSearch = new System.Windows.Forms.Label();
+            this.lblCategoryId = new System.Windows.Forms.Label();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.panelRight = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategories)).BeginInit();
+            this.panelLeft.SuspendLayout();
+            this.panelRight.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridViewCategories
+            // 
+            this.dataGridViewCategories.AllowUserToAddRows = false;
+            this.dataGridViewCategories.AllowUserToDeleteRows = false;
+            this.dataGridViewCategories.AllowUserToOrderColumns = true;
+            this.dataGridViewCategories.AllowUserToResizeRows = false;
+            this.dataGridViewCategories.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewCategories.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridViewCategories.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewCategories.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle7.BackColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridViewCategories.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dataGridViewCategories.ColumnHeadersHeight = 40;
+            dataGridViewCellStyle8.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle8.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            dataGridViewCellStyle8.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle8.SelectionBackColor = System.Drawing.Color.LightSteelBlue;
+            dataGridViewCellStyle8.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle8.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridViewCategories.DefaultCellStyle = dataGridViewCellStyle8;
+            this.dataGridViewCategories.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewCategories.EnableHeadersVisualStyles = false;
+            this.dataGridViewCategories.GridColor = System.Drawing.Color.Gainsboro;
+            this.dataGridViewCategories.Location = new System.Drawing.Point(0, 0);
+            this.dataGridViewCategories.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewCategories.MultiSelect = false;
+            this.dataGridViewCategories.Name = "dataGridViewCategories";
+            this.dataGridViewCategories.ReadOnly = true;
+            this.dataGridViewCategories.RowHeadersVisible = false;
+            this.dataGridViewCategories.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewCategories.Size = new System.Drawing.Size(707, 612);
+            this.dataGridViewCategories.TabIndex = 0;
+            this.dataGridViewCategories.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewCategories_CellClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 155);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(103, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Category Name:";
+            // 
+            // txtCategoryName
+            // 
+            this.txtCategoryName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCategoryName.Location = new System.Drawing.Point(20, 180);
+            this.txtCategoryName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCategoryName.Name = "txtCategoryName";
+            this.txtCategoryName.Size = new System.Drawing.Size(260, 25);
+            this.txtCategoryName.TabIndex = 0;
+            // 
+            // btnList
+            // 
+            this.btnList.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnList.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnList.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnList.ForeColor = System.Drawing.Color.White;
+            this.btnList.Location = new System.Drawing.Point(20, 250);
+            this.btnList.Margin = new System.Windows.Forms.Padding(4);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(260, 45);
+            this.btnList.TabIndex = 4;
+            this.btnList.Text = "LIST";
+            this.btnList.UseVisualStyleBackColor = false;
+            this.btnList.Click += new System.EventHandler(this.btnList_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.btnAdd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAdd.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAdd.ForeColor = System.Drawing.Color.White;
+            this.btnAdd.Location = new System.Drawing.Point(20, 305);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(4);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(260, 45);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.BackColor = System.Drawing.Color.Orange;
+            this.btnUpdate.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnUpdate.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnUpdate.ForeColor = System.Drawing.Color.White;
+            this.btnUpdate.Location = new System.Drawing.Point(20, 360);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(4);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(260, 45);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.BackColor = System.Drawing.Color.Crimson;
+            this.btnDelete.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDelete.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnDelete.ForeColor = System.Drawing.Color.White;
+            this.btnDelete.Location = new System.Drawing.Point(20, 415);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(4);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(260, 45);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = false;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // txtCategoryId
+            // 
+            this.txtCategoryId.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtCategoryId.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtCategoryId.Location = new System.Drawing.Point(20, 105);
+            this.txtCategoryId.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCategoryId.Name = "txtCategoryId";
+            this.txtCategoryId.ReadOnly = true;
+            this.txtCategoryId.Size = new System.Drawing.Size(260, 25);
+            this.txtCategoryId.TabIndex = 7;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.panelLeft.Controls.Add(this.btnSearch);
+            this.panelLeft.Controls.Add(this.txtSearch);
+            this.panelLeft.Controls.Add(this.lblSearch);
+            this.panelLeft.Controls.Add(this.btnDelete);
+            this.panelLeft.Controls.Add(this.lblCategoryId);
+            this.panelLeft.Controls.Add(this.btnUpdate);
+            this.panelLeft.Controls.Add(this.lblTitle);
+            this.panelLeft.Controls.Add(this.label1);
+            this.panelLeft.Controls.Add(this.btnList);
+            this.panelLeft.Controls.Add(this.txtCategoryId);
+            this.panelLeft.Controls.Add(this.btnAdd);
+            this.panelLeft.Controls.Add(this.txtCategoryName);
+            this.panelLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panelLeft.Location = new System.Drawing.Point(0, 0);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(320, 612);
+            this.panelLeft.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.BackColor = System.Drawing.Color.SlateBlue;
+            this.btnSearch.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSearch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSearch.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSearch.ForeColor = System.Drawing.Color.White;
+            this.btnSearch.Location = new System.Drawing.Point(20, 555);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(260, 45);
+            this.btnSearch.TabIndex = 11;
+            this.btnSearch.Text = "SEARCH";
+            this.btnSearch.UseVisualStyleBackColor = false;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSearch.Location = new System.Drawing.Point(20, 505);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(260, 25);
+            this.txtSearch.TabIndex = 10;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // lblSearch
+            // 
+            this.lblSearch.AutoSize = true;
+            this.lblSearch.Location = new System.Drawing.Point(20, 480);
+            this.lblSearch.Name = "lblSearch";
+            this.lblSearch.Size = new System.Drawing.Size(47, 17);
+            this.lblSearch.TabIndex = 9;
+            this.lblSearch.Text = "Search";
+            // 
+            // lblCategoryId
+            // 
+            this.lblCategoryId.AutoSize = true;
+            this.lblCategoryId.Location = new System.Drawing.Point(20, 80);
+            this.lblCategoryId.Name = "lblCategoryId";
+            this.lblCategoryId.Size = new System.Drawing.Size(79, 17);
+            this.lblCategoryId.TabIndex = 8;
+            this.lblCategoryId.Text = "Category Id:";
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTitle.Location = new System.Drawing.Point(20, 20);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(217, 25);
+            this.lblTitle.TabIndex = 0;
+            this.lblTitle.Text = "Category Management";
+            // 
+            // panelRight
+            // 
+            this.panelRight.Controls.Add(this.dataGridViewCategories);
+            this.panelRight.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelRight.Location = new System.Drawing.Point(320, 0);
+            this.panelRight.Name = "panelRight";
+            this.panelRight.Size = new System.Drawing.Size(707, 612);
+            this.panelRight.TabIndex = 9;
+            // 
+            // FrmCategory
+            // 
+            this.AcceptButton = this.btnAdd;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1027, 612);
+            this.Controls.Add(this.panelRight);
+            this.Controls.Add(this.panelLeft);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "FrmCategory";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Category Management";
+            this.Load += new System.EventHandler(this.FrmCategory_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCategories)).EndInit();
+            this.panelLeft.ResumeLayout(false);
+            this.panelLeft.PerformLayout();
+            this.panelRight.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewCategories;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCategoryName;
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.TextBox txtCategoryId;
+        private System.Windows.Forms.Panel panelLeft;
+        private System.Windows.Forms.Panel panelRight;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblCategoryId;
+        private System.Windows.Forms.Label lblSearch;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button btnSearch;
+    }
+}
+
---
+++ StockTrackingSystem/FrmCategory.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace StockTrackingSystem
+{
+    public partial class FrmCategory : Form
+    {
+        string connectionString = @"Server=localhost\SQLEXPRESS;Database=StockDb;Trusted_Connection=True;";
+
+        void CategoryList()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            SqlDataAdapter da = new SqlDataAdapter("SELECT CategoryId, CategoryName FROM Categories", connection);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridViewCategories.DataSource = dt;
+        }
+
+        public FrmCategory()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmCategory_Load(object sender, EventArgs e)
+        {
+            CategoryList();
+        }
+
+        private void btnList_Click(object sender, EventArgs e)
+        {
+            CategoryList();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategoryName.Text))
+            {
+                MessageBox.Show("Category name cannot be empty!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("INSERT INTO Categories (CategoryName) VALUES (@p1)", connection);
+            command.Parameters.AddWithValue("@p1", txtCategoryName.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Category added successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            CategoryList();
+            txtCategoryName.Clear();
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategoryId.Text))
+            {
+                MessageBox.Show("Please select a category from the table first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtCategoryName.Text))
+            {
+                MessageBox.Show("Category name cannot be empty!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("UPDATE Categories SET CategoryName=@p1 WHERE CategoryId=@p2", connection);
+            command.Parameters.AddWithValue("@p1", txtCategoryName.Text);
+            command.Parameters.AddWithValue("@p2", txtCategoryId.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Category updated successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            CategoryList();
+            txtCategoryId.Clear();
+            txtCategoryName.Clear();
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtCategoryId.Text))
+            {
+                MessageBox.Show("Please select a category from the table first!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Are you sure you want to delete this category?",
+                "Confirm Delete",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            if (result == DialogResult.No)
+                return;
+
+            SqlConnection connection = new SqlConnection(connectionString);
+            connection.Open();
+
+            SqlCommand command = new SqlCommand("DELETE FROM Categories WHERE CategoryId=@p1", connection);
+            command.Parameters.AddWithValue("@p1", txtCategoryId.Text);
+
+            command.ExecuteNonQuery();
+            connection.Close();
+
+            MessageBox.Show("Category deleted successfully!", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            CategoryList();
+            txtCategoryId.Clear();
+            txtCategoryName.Clear();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtSearch.Text))
+            {
+                CategoryList();
+                return;
+            }
+
+            SqlConnection connection = new SqlConnection(connectionString);
+
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT CategoryId, CategoryName FROM Categories WHERE CategoryName LIKE '%' + @p1 + '%'",
+                connection
+            );
+
+            da.SelectCommand.Parameters.AddWithValue("@p1", txtSearch.Text);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dataGridViewCategories.DataSource = dt;
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            btnSearch.PerformClick();
+        }
+
+        private void dataGridViewCategories_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int selectedRow = dataGridViewCategories.SelectedCells[0].RowIndex;
+
+            txtCategoryId.Text = dataGridViewCategories.Rows[selectedRow].Cells[0].Value.ToString();
+            txtCategoryName.Text = dataGridViewCategories.Rows[selectedRow].Cells[1].Value.ToString();
+        }
+
+    }
+}
